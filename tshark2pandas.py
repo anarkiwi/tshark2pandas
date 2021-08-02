ce061dfea063a7b27f78756d3fd48cd7256ee7e4
Josh Bailey <josh@vandervecken.com>
2021-08-02 06:30:27 +0000

    rf.

--- tshark2pandas.py
+++ tshark2pandas.py
@@ -4,21 +4,30 @@ import pandas as pd
 import subprocess
 import sys
 
+TSHARK_ARGS = ['-Tek', '-n']
+JQ_REMAP_SCRIPT = r'def map_values(f): with_entries(.value = (.value|f)); .[].layers|flatten|add|del(.filtered)|map_values(tonumber? // .)'
+JQ_STREAM_REMAP_SCRIPT = r'fromstream(1|truncate_stream(inputs))|flatten|add'
+
+
 def tshark2pandas(pcap_filename, layers=None, snaplen=256):
     if layers is None:
        layers = ['eth', 'ip', 'frame']
     layer_args = []
     if layers:
         layer_args = ['-J', ' '.join(layers)]
-    if not snaplen:
-        tshark_proc = subprocess.Popen(['tshark', '-r', pcap_filename, '-Tek', '-n'] + layer_args, stdout=subprocess.PIPE)
+
+    procs = []
+    if snaplen:
+        procs.append(subprocess.Popen(['editcap', '-s', str(snaplen), '-F', 'pcap', pcap_filename, '-'], stdout=subprocess.PIPE))
+        procs.append(subprocess.Popen(['tshark', '-r', '-'] + TSHARK_ARGS + layer_args, stdin=procs[-1].stdout, stdout=subprocess.PIPE))
     else:
-        editcap_proc = subprocess.Popen(['editcap', '-s', str(snaplen), '-F', 'pcap', pcap_filename, '-'], stdout=subprocess.PIPE)
-        tshark_proc = subprocess.Popen(['tshark', '-r', '-', '-Tek', '-n'] + layer_args, stdin=editcap_proc.stdout, stdout=subprocess.PIPE)
-    grep_proc = subprocess.Popen(['grep', '-v', '_index'], stdin=tshark_proc.stdout, stdout=subprocess.PIPE)
-    # jq_proc = subprocess.Popen(['jq', '-nc', '--stream', 'fromstream(1|truncate_stream(inputs))|flatten|add'], stdin=grep_proc.stdout, stdout=subprocess.PIPE)
-    jq_proc = subprocess.Popen(['jq', '-c', '-s', 'def map_values(f): with_entries(.value = (.value|f)); .[].layers|flatten|add|del(.filtered)|map_values(tonumber? // .)'], stdin=grep_proc.stdout, stdout=subprocess.PIPE)
-    return pd.read_json(jq_proc.stdout, lines=True)
+        procs.append(subprocess.Popen(['tshark', '-r', pcap_filename] + TSHARK_ARGS + layer_args, stdout=subprocess.PIPE))
+    procs.append(subprocess.Popen(['grep', '-v', '_index'], stdin=procs[-1].stdout, stdout=subprocess.PIPE))
+    procs.append(subprocess.Popen(['jq', '-c', '-s', JQ_REMAP_SCRIPT], stdin=procs[-1].stdout, stdout=subprocess.PIPE))
+    df = pd.read_json(procs[-1].stdout, lines=True)
+    for proc in procs:
+        proc.wait()
+    return df
 
 
 def main():

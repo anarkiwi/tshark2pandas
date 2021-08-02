dab4100695e0b15795913dfe508788bae3916ae1
Josh Bailey <josh@vandervecken.com>
2021-08-02 07:33:24 +0000

    hex/int.

--- tshark2pandas.py
+++ tshark2pandas.py
@@ -5,8 +5,9 @@ import subprocess
 import sys
 
 TSHARK_ARGS = ['-Tek', '-n']
-JQ_REMAP_SCRIPT = r'def map_values(f): with_entries(.value = (.value|f)); .[].layers|flatten|add|del(.filtered)|map_values(tonumber? // .)'
-JQ_STREAM_REMAP_SCRIPT = r'fromstream(1|truncate_stream(inputs))|flatten|add'
+#JQ_REMAP_SCRIPT = r'def map_values(f): with_entries(.value = (.value|f)); .[].layers|flatten|add|del(.filtered)|map_values(tonumber? // .)'
+#JQ_STREAM_REMAP_SCRIPT = r'fromstream(1|truncate_stream(inputs))|flatten|add'
+JQ_REMAP_SCRIPT = r'.[].layers|flatten|add|del(.filtered)'
 
 
 def tshark2pandas(pcap_filename, layers=None, snaplen=256):
@@ -25,11 +26,19 @@ def tshark2pandas(pcap_filename, layers=None, snaplen=256):
     procs.append(subprocess.Popen(['grep', '-v', '_index'], stdin=procs[-1].stdout, stdout=subprocess.PIPE))
     procs.append(subprocess.Popen(['jq', '-c', '-s', JQ_REMAP_SCRIPT], stdin=procs[-1].stdout, stdout=subprocess.PIPE))
     df = pd.read_json(procs[-1].stdout, lines=True)
+    # TODO: ideally, detect and convert hex ints before pandas.
     object_cols = {col for col, col_dtype in df.dtypes.iteritems() if col_dtype == object}
     for col in object_cols:
+        # hex strings
         prefixes = df[col].str[:2]
-        if prefixes[0] == '0x' and not prefixes[prefixes != '0x'].any():
-            df[col] = df[col].map(lambda x: int(x, 16))
+        if prefixes[0] == '0x':
+            if prefixes.nunique() == 1:
+                df[col] = df[col].fillna('0x0').map(lambda x: int(x, 16)).astype(pd.Int64Dtype())
+                continue
+        # int strings
+        if df[col][(df[col].notna()) & (df[col].str.isdecimal())].size == df[col].size:
+            df[col] = df[col].fillna('0').map(int).astype(pd.Int64Dtype())
+            continue
     for proc in procs:
         proc.wait()
     return df

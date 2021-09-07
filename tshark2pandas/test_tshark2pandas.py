82df81838ed9ded060f51450f4d5d7744fdb26d9
Josh Bailey <josh@vandervecken.com>
2021-09-07 08:32:27 +0000

    col rename.

--- tshark2pandas/test_tshark2pandas.py
+++ tshark2pandas/test_tshark2pandas.py
@@ -12,7 +12,7 @@ class Tshark2PandasTestCase(unittest.TestCase):
     def test_tshark2pandas(self):
         df = tshark2pandas(os.path.join(os.path.dirname(__file__), 'test.cap'))
         self.assertEqual(10, len(df))
-        self.assertEqual(10 * 98, df['frame.frame_cap_len'].sum())
+        self.assertEqual(10 * 98, df['frame.cap_len'].sum(), list(df.columns))
     
 
 if __name__ == '__main__':

fe2cfdaf792799d9805e59752fe8bef0014de0df
Josh Bailey <josh@vandervecken.com>
2021-09-09 03:04:18 +0000

    revert to pbr.

--- setup.py
+++ setup.py
@@ -2,22 +2,12 @@
 
 import setuptools
 import subprocess
-from distutils.command.build_py import build_py
-
-
-class BuildCommand(build_py):
-
-    def run(self):
-        command = ['make', '--directory=tshark2pandas']
-        subprocess.check_call(command)
-        super().run()
-
 
 setuptools.setup(
     name='tshark2pandas',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     python_requires='>=3.8',
+    pbr=True,
     packages=['tshark2pandas'],
     package_data={'': ['tshark2pandas_jsonfilter']},
-    cmdclass={'build_py': BuildCommand},
 )

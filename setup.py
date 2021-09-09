08b34a64cdbdcf368eda791ec99034dc7a442712
Josh Bailey <josh@vandervecken.com>
2021-09-09 01:49:19 +0000

    setup install.

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
-from setuptools import setup
 
-setup(
+import setuptools
+import subprocess
+from distutils.command.build_py import build_py
+
+
+class BuildCommand(build_py):
+
+    def run(self):
+        command = ['make', '--directory=tshark2pandas']
+        subprocess.check_call(command)
+        super().run()
+
+
+setuptools.setup(
     name='tshark2pandas',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
     python_requires='>=3.8',
     packages=['tshark2pandas'],
-    pbr=True,
-    package_data={"": ["tshark2pandas_jsonfilter"]},
+    package_data={'': ['tshark2pandas_jsonfilter']},
+    cmdclass={'build_py': BuildCommand},
 )
